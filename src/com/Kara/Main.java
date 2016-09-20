09b0a1cbd909d47cb8b934c423b7a518ac477674
cats <cats@jellybeans.com>
2016-09-20 12:16:00 -0500

    Added more comments to the code

--- src/com/Kara/Main.java
+++ src/com/Kara/Main.java
@@ -9,17 +9,18 @@ class Main {
         try (BufferedReader bufReader = new BufferedReader(new FileReader(filename));) {
 
            BufferedWriter bufWriter = new BufferedWriter(new FileWriter("Kara.txt"));
-
-
-
+            //write this data to the file
            bufWriter.write("Kara \r\n");
            bufWriter.write("Blue \r\n");
            bufWriter.write(2425 + "\r\n");
            bufWriter.close();
 
+            //tell the bufReader to read the next line
            String line = bufReader.readLine();
 
-
+            /*while line is not equal to null, continue printing
+            out each line from the file to the console
+            */
            while (line != null) {
                System.out.printf(line);
 
@@ -28,7 +29,7 @@ class Main {
 
            bufReader.close();
        } catch (IOException ioe){
-           System.out.println("Could not open or read " + filename);
+           System.out.println("Could not open or read " + filename + "\r\n");
            System.out.println(ioe.toString());
        }
         System.out.println("If the code did not work, the program did not crash");
