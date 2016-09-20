4e469411b36c0b5dbee1a137aa402352b158898e
cats <cats@jellybeans.com>
2016-09-20 12:40:46 -0500

    Added main method for problem 3 and added a number scanner

--- src/com/Kara/Problem3.java
+++ src/com/Kara/Problem3.java
@@ -1,7 +1,14 @@
 package com.Kara;
 
+import java.util.Scanner;
+
 /**
  * Created by stiri on 9/20/2016.
  */
 public class Problem3 {
+    private static Scanner numberScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
 }
